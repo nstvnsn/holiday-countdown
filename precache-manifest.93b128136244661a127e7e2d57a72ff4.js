269ea376cfe451dd6bab00d510af36c55800b2cb
Nathan Stevenson <nscstevenson@mun.ca>
2020-10-09 10:23:57 -0600

    Updates

---
+++ precache-manifest.93b128136244661a127e7e2d57a72ff4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c82de379de6bbfb65dd28da09c5ba2",
+    "url": "/holiday-countdown/index.html"
+  },
+  {
+    "revision": "2699d034be2bf1475bd8",
+    "url": "/holiday-countdown/static/css/main.8514891c.chunk.css"
+  },
+  {
+    "revision": "5f3c18d81ccc2464b822",
+    "url": "/holiday-countdown/static/js/2.9fa7aa8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/holiday-countdown/static/js/2.9fa7aa8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2699d034be2bf1475bd8",
+    "url": "/holiday-countdown/static/js/main.7ecaa533.chunk.js"
+  },
+  {
+    "revision": "7572d4a27b7ddd77cd31",
+    "url": "/holiday-countdown/static/js/runtime-main.a1a76005.js"
+  },
+  {
+    "revision": "4525d06f428866a82ac00e2e9d4258ab",
+    "url": "/holiday-countdown/static/media/OneStarryNight-lp45.4525d06f.ttf"
+  }
+]);
